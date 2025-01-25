c786c6f036fe80732f82e3e6790a66d02e917379
adam <adamsoro0321@gmail.com>
2025-01-25 05:14:25 -0500

    all crud done

--- service/routes.py
+++ service/routes.py
@@ -70,6 +70,7 @@ def list_accounts():
         """
         List all Accounts
         This endpoint will list all Accounts
+        
         """
         app.logger.info("Request to list Accounts")
 
